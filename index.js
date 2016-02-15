5adeed58ff6c8bf551b5853d9e4a1c38b0a6fc4c
Joe Groseclose <joegroseclose@Joes-MBP.home>
2016-02-14 21:33:05 -0500

    Updated to include the noCompact option

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var istanbul = require('istanbul');
 module.exports = function(source) {
     var instrumenter = new istanbul.Instrumenter({
         embedSource: true,
-        noAutoWrap: true
+        noAutoWrap: true,
+        noCompact: true
     });
 
     if (this.cacheable) {
